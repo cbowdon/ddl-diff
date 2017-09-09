a63ac6c893dabc7a17ad5546645b1502da063e88
Chris Bowdon <c.bowdon@bath.edu>
2017-09-09 23:15:15 +0100

    Refactor AST to have heirarchical structure

--- src/main/scala/me/bowdon/DDLParser.scala
+++ src/main/scala/me/bowdon/DDLParser.scala
@@ -7,13 +7,19 @@ abstract class SQLType
 case object Number extends SQLType
 case object Text extends SQLType
 
-abstract class DDLExpression
-case object Create extends DDLExpression
-case class Table(name: String) extends DDLExpression
-// TODO: constraints
-case class Column(name: String, sqlType: SQLType) extends DDLExpression
-case class Columns(defs: Column*) extends DDLExpression
-case object Terminator extends DDLExpression
+case class ColumnConstraint()
+
+case class TableConstraint()
+
+case class ColumnDef(
+  name: String,
+  sqlType: SQLType,
+  constraints: Seq[ColumnConstraint])
+
+case class TableDef(
+  name: String,
+  columns: Seq[ColumnDef],
+  constraints: Seq[TableConstraint])
 
 class ParseError(reason: String) {
   override def toString = reason
@@ -21,38 +27,35 @@ class ParseError(reason: String) {
 
 class DDLParser extends RegexParsers {
 
-  def create: Parser[DDLExpression] = "(?i)create".r ^^ { _ => Create }
+  def create: Parser[String] = "(?i)create".r
 
   // TODO: ANSI quotes
   def identifier: Parser[String] = "[A-Za-z_][0-9A-Za-z_]+".r ^^ { _.toString }
-
-  def table: Parser[DDLExpression] = {
-    "(?i)table".r ~> identifier ^^ { Table(_) }
-  }
 
   def sqlType: Parser[SQLType] = {
     "(?i)number".r ^^ { _ => Number } |
     "(?i)text".r ^^ { _ => Text }
   }
 
-  def column: Parser[Column] = {
-    identifier ~ sqlType ^^ { case name ~ sqlType => Column(name, sqlType) }
+  def column: Parser[ColumnDef] = {
+    identifier ~ sqlType ^^ { case name ~ sqlType => ColumnDef(name, sqlType, Seq.empty) }
   }
 
-  def columns: Parser[DDLExpression] = {
-    val colParsers = "(" ~> (column <~ ",".? ).* <~ ")" ^^ identity
-    colParsers.map(cols => Columns(cols: _*))
-  }
+  def columns: Parser[Seq[ColumnDef]] = "(" ~> (column <~ ",".? ).* <~ ")"
 
-  def terminator: Parser[DDLExpression] = ";" ^^ { _ => Terminator }
-
-  def expr: Parser[Seq[DDLExpression]] = {
-    create ~ table ~ columns ~ terminator.? ^^ {
-      case create ~ table ~ columns ~ _ => Seq(create, table, columns)
+  def table: Parser[TableDef] = {
+    "(?i)table".r ~> identifier ~ columns ^^ {
+      case name ~ cols => TableDef(name, cols, Seq.empty)
     }
   }
 
-  def apply(input: String): Either[ParseError, Seq[DDLExpression]] = {
+  def terminator: Parser[String] = ";"
+
+  def expr: Parser[TableDef] = {
+    create ~> table <~ terminator.?
+  }
+
+  def apply(input: String): Either[ParseError, TableDef] = {
     // Either is standing in for a type I have yet to design
     parseAll(expr, input) match {
       case Success(result, _) => Right(result)
--- src/test/scala/me/bowdon/DDLParserSpec.scala
+++ src/test/scala/me/bowdon/DDLParserSpec.scala
@@ -4,26 +4,48 @@ import org.scalatest._
 import org.scalatest.Matchers._
 
 class DDLParserSpec extends FlatSpec with Matchers {
-    val parser = new DDLParser
+  val parser = new DDLParser
 
   "The DDLParser class" should "parse a simple create" in {
 
-    parser.apply("create table foo (id number);") shouldEqual Right(Seq(Create, Table("foo"), Columns(Column("id", Number))))
+    parser.apply("create table foo (id number);") shouldEqual
+    Right(
+      TableDef(
+        "foo",
+        Seq(ColumnDef("id", Number, Seq.empty)),
+        Seq.empty))
   }
 
   it should "parse multiple columns" in {
 
-    parser.apply("create table foo (id number, name text);") shouldEqual Right(Seq(Create, Table("foo"), Columns(Column("id", Number), Column("name", Text))))
+    parser.apply("create table foo (id number, name text);") shouldEqual
+    Right(
+      TableDef(
+        "foo",
+        Seq(
+          ColumnDef("id", Number, Seq.empty),
+          ColumnDef("name", Text, Seq.empty)),
+        Seq.empty))
   }
 
   it should "not care if people are shouting" in {
 
-    parser.apply("CREATE TABLE foo (id NUMBER);") shouldEqual Right(Seq(Create, Table("foo"), Columns(Column("id", Number))))
+    parser.apply("CREATE TABLE foo (id NUMBER);") shouldEqual
+    Right(
+      TableDef(
+        "foo",
+        Seq(ColumnDef("id", Number, Seq.empty)),
+        Seq.empty))
   }
 
   it should "not mind a missing terminator" in {
 
-    parser.apply("CREATE TABLE foo (id NUMBER)") shouldEqual Right(Seq(Create, Table("foo"), Columns(Column("id", Number))))
+    parser.apply("CREATE TABLE foo (id NUMBER)") shouldEqual
+    Right(
+      TableDef(
+        "foo",
+        Seq(ColumnDef("id", Number, Seq.empty)),
+        Seq.empty))
   }
 
   it should "fail to parse invalid syntax" in {
