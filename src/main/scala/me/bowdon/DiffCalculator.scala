8690e58792d3c1f41a2386dcfa4dfdc36b4c5296
Chris Bowdon <c.bowdon@bath.edu>
2017-09-13 22:16:13 +0100

    Add DiffCalculator types and tests

---
+++ src/main/scala/me/bowdon/DiffCalculator.scala
@@ -0,0 +1,28 @@
+package me.bowdon
+
+abstract class Migration
+
+// Table migrations
+case class CreateTable(table: TableDef) extends Migration
+case class DropTable(tableName: String) extends Migration
+case class RenameTable(oldTableName: String, newTableName: String) extends Migration
+
+// Column migrations
+case class AddColumn(tableName: String, column: ColumnDef) extends Migration
+case class DropColumn(tableName: String, column: ColumnDef) extends Migration
+case class RenameColumn(tableName: String, column: ColumnDef) extends Migration
+
+// Column constraint migrations
+case class AddColumnConstraint(tableName: String, columnName: String, constraint: ColumnConstraint) extends Migration
+case class DropColumnConstraint(tableName: String, columnName: String, constraint: ColumnConstraint) extends Migration
+
+// Table constraint migrations
+case class AddTableConstraint(tableName: String, constraint: TableConstraint) extends Migration
+case class DropTableConstraint(tableName: String, constraint: TableConstraint) extends Migration
+
+object DiffCalculator {
+
+  def diff(oldTable: Option[TableDef], newTable: Option[TableDef]): Seq[Migration] = {
+    ???
+  }
+}
---
+++ src/test/scala/me/bowdon/DiffCalculatorSpec.scala
@@ -0,0 +1,105 @@
+package me.bowdon
+
+import org.scalatest._
+import org.scalatest.Matchers._
+
+class DiffCalculatorSpec extends FlatSpec with Matchers {
+
+  "DiffCalculator" should "create missing tables" in {
+
+    val idCol = ColumnDef("id", Numeric, Set.empty)
+    val newTable = TableDef("foo", Map("id" -> idCol), Set())
+
+    DiffCalculator.diff(None, Some(newTable)) shouldEqual
+    Seq(
+      CreateTable(newTable))
+  }
+
+  it should "drop removed tables" in {
+
+    val idCol = ColumnDef("id", Numeric, Set.empty)
+    val oldTable = TableDef("foo", Map("id" -> idCol), Set())
+
+    DiffCalculator.diff(Some(oldTable), None) shouldEqual
+    Seq(
+      DropTable("foo")
+    )
+  }
+
+  it should "add missing columns" in {
+
+    val oldTable = TableDef("foo", Map(), Set())
+
+    val idCol = ColumnDef("id", Numeric, Set.empty)
+    val newTable = oldTable.copy(columns = Map("id" -> idCol))
+
+    DiffCalculator.diff(Some(oldTable), Some(newTable)) shouldEqual
+    Seq(
+      AddColumn("foo", idCol)
+    )
+  }
+
+  it should "drop removed columns" in {
+
+    val idCol = ColumnDef("id", Numeric, Set.empty)
+    val oldTable = TableDef("foo", Map("id" -> idCol), Set())
+
+    val newTable = oldTable.copy(columns = Map())
+
+    DiffCalculator.diff(Some(oldTable), Some(newTable)) shouldEqual
+    Seq(
+      DropColumn("foo", idCol)
+    )
+  }
+
+  it should "add new column constraints" in {
+
+    val oldIdCol = ColumnDef("id", Numeric, Set.empty)
+    val constraint = ColumnConstraint(None, PrimaryKey(Some(Asc), true))
+    val newIdCol = oldIdCol.copy(constraints = Set(constraint))
+
+    val oldTable = TableDef("foo", Map("id" -> oldIdCol), Set())
+
+    val newTable = oldTable.copy(columns = Map("id" -> newIdCol))
+
+    DiffCalculator.diff(Some(oldTable), Some(newTable)) shouldEqual
+    Seq(
+      AddColumnConstraint("foo", "id", constraint)
+    )
+  }
+
+  it should "drop removed column constraints" in {
+
+    val constraint = ColumnConstraint(None, PrimaryKey(Some(Asc), true))
+    val oldIdCol = ColumnDef("id", Numeric, Set(constraint))
+    val newIdCol = oldIdCol.copy(constraints = Set())
+
+    val oldTable = TableDef("foo", Map("id" -> oldIdCol), Set())
+
+    val newTable = oldTable.copy(columns = Map("id" -> newIdCol))
+
+    DiffCalculator.diff(Some(oldTable), Some(newTable)) shouldEqual
+    Seq(
+      DropColumnConstraint("foo", "id", constraint)
+    )
+  }
+
+  it should "assume a constraint rename is a drop and create" in {
+
+    val oldConstraint = ColumnConstraint(Some("foo_pk"), PrimaryKey(Some(Asc), true))
+    val newConstraint = ColumnConstraint(Some("foo_pkx"), PrimaryKey(Some(Asc), true))
+    val idCol = ColumnDef("id", Numeric, Set(oldConstraint))
+    val oldTable = TableDef("foo", Map("id" -> idCol), Set())
+    val newTable = oldTable.copy(
+      columns = Map("id" -> idCol.copy(
+        constraints = Set(newConstraint))
+      )
+    )
+
+    DiffCalculator.diff(Some(oldTable), Some(newTable)) shouldEqual
+    Seq(
+      DropColumnConstraint("foo", "id", oldConstraint),
+      AddColumnConstraint("foo", "id", newConstraint)
+    )
+  }
+}
